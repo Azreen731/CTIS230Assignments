ae9613ac3c481b639efe999eb9b726aa35b2c5f0
Azreen731 <56175596+Azreen731@users.noreply.github.com>
2022-08-27 12:52:18 -0400

    changes made for test javascript

--- javascripttest/testjs.js
+++ javascripttest/testjs.js
@@ -32,4 +32,42 @@ ourButton.addEventListener("click", onClick);
 //Write the event handler
 function OnClick() {
     ourText.innerHTML = "I clicked the button";
+    ourText.style.fontSize="2em";
+    myCard.style.display = "block"; //now make the image appear by changing display from "none" to "block"
+
 }
+myCard = document.getElementById("card"); //get the div that holds the card image
+
+
+//Get the textfield within the id myinput
+ourInput = document.getElementById("my input"); //This contains the new text
+ourHeading = document.getElementById("header"); //What we're going to modify
+
+//Tell the textfield what event listener is connected to its events
+
+ourInput.addEventListener("change", changeText); //The text in the field changes
+ourInput.addEventListener("blur", changeText); //The text field loses focus
+// when we click out of it, the text field loses focus, and that generates a blur event
+
+function changeText() {
+  //Get the text that the user entered
+  newtext = ourInput.value; //we ask ourInput for its value, which is the entered text
+  myheading.innerHTML = newtext;
+}
+
+myCardImage = document.getElementById("cardimage");
+myCard.addEventListener("click", changeImage);
+function changeImage() {
+  if (myCard.style.position == "") {
+    
+  }
+  myCard.style.position("absolute"); //make the position of the div absolute and 100px from the top left corner
+  myCard.style.top("100px");
+  myCard.style.left("100px");
+  myCardImage.style.width("80px"); // make the width of the image 80px
+  // not the width of the div bc that won't affect how wide the image is
+  myCardImage.src="images/2_of_diamonds.png";
+}
+
+
+
