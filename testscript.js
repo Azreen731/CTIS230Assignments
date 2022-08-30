08b96a7d2042938b18d306ce80be929d36bafdbd
Azreen731 <56175596+Azreen731@users.noreply.github.com>
2022-08-30 14:50:59 -0400

    thumbnail example

--- testscript.js
+++ testscript.js
@@ -1,7 +1,9 @@
 myitem = document.getElementById("firsttest");
-
+//we asked html document for item with id "firsttest" and then we stored it in myitem//
 myitem.addEventListener("click", onClick);
-
+//added event listener to change behavior when text is clicked
 function onClick() {
     myitem.style.color="green";
-}
+    myitem.style.backgroundColor="aliceblue";
+}   
+//we've decided to change the text style (color) to green when it is clicked on
