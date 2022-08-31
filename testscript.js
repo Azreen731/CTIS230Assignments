134566e61d546c0bdb868e051a6ff85333c7975a
Azreen731 <56175596+Azreen731@users.noreply.github.com>
2022-08-30 21:16:23 -0400

    assignment 6

--- testscript.js
+++ testscript.js
@@ -7,3 +7,41 @@ function onClick() {
     myitem.style.backgroundColor="aliceblue";
 }   
 //we've decided to change the text style (color) to green when it is clicked on
+
+myitem.addEventListener("mouseout", onmouseout) ;
+
+function onmouseout() {
+    myitem.style.color="";
+    myitem.style.backgroundColor="";
+}
+
+thebutton = document.getElementById("thebutton");
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {
+    otheritem.style.color = "gold";
+    otheritem.style.backgroundColor = "ghostwhite";
+}
+
+offbutton = document.getElementById("offbutton");
+oneitem = document.getElementById("buttonlight");
+offbuton = document.addEventListener("click", onButtonClick);
+function onButtonClick() {
+    oneitem.style.color = "lightgrey";
+}
+
+textentry = document.getElementById("myinput");
+textentry.addEventListener("change",onChange);
+function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
+}
+
+//extra function
+
+oneitem.addEventListener("click", onClick)
+function onClick() {
+    oneitem.style.color="blue";
+}
+
