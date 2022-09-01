34c215c7d1499e9c07be335fbad3f1528d487bc3
Azreen731 <56175596+Azreen731@users.noreply.github.com>
2022-09-01 16:01:12 -0400

    final version of ass. 6 - all the buttons work

--- testscript.js
+++ testscript.js
@@ -1,47 +1,62 @@
+function onClick() {
+    onClick1();
+    onClick2();
+}
+
 myitem = document.getElementById("firsttest");
 //we asked html document for item with id "firsttest" and then we stored it in myitem//
-myitem.addEventListener("click", onClick);
+myitem.addEventListener("click", onClick1);
 //added event listener to change behavior when text is clicked
-function onClick() {
+function onClick1() {
     myitem.style.color="green";
     myitem.style.backgroundColor="aliceblue";
-}   
-//we've decided to change the text style (color) to green when it is clicked on
-
+} 
 myitem.addEventListener("mouseout", onmouseout) ;
 
 function onmouseout() {
     myitem.style.color="";
     myitem.style.backgroundColor="";
-}
-
-thebutton = document.getElementById("thebutton");
-otheritem = document.getElementById("buttontest");
-
-thebutton.addEventListener("click", onButtonClick);
-function onButtonClick() {
-    otheritem.style.color = "gold";
-    otheritem.style.backgroundColor = "ghostwhite";
-}
-
-offbutton = document.getElementById("offbutton");
-oneitem = document.getElementById("buttonlight");
-offbuton = document.addEventListener("click", onButtonClick);
-function onButtonClick() {
-    oneitem.style.color = "lightgrey";
-}
-
-textentry = document.getElementById("myinput");
-textentry.addEventListener("change",onChange);
-function onChange() {
-    newtext = myinput.value;
-    otheritem.innerHTML = newtext;
-}
+}   
+//we've decided to change the text style (color) to green when it is clicked on
 
 //extra function
+ thing = document.getElementById("judge");
+ thing.addEventListener("click", onClick2)
+ function onClick2() {
+     thing.style.color="blue";
+ }
 
-oneitem.addEventListener("click", onClick)
-function onClick() {
-    oneitem.style.color="blue";
-}
+
+function onButtonClick() {
+     onButtonClick1();
+     onButtonClick2();
+ }
+
+ thebutton = document.getElementById("thebutton");
+ otheritem = document.getElementById("buttontest");
+
+ thebutton.addEventListener("click", onButtonClick1);
+ 
+ function onButtonClick1() {
+     otheritem.style.color = "gold";
+     otheritem.style.backgroundColor = "ghostwhite";
+ }
+ thebutton2 = document.getElementById("thebutton2");
+ lastitem = document.getElementById("buttontest2");
+
+ thebutton2.addEventListener("click", onButtonClick2);
+ 
+ function onButtonClick2() {
+     lastitem.style.color = "lightgrey";
+ }
+
+ textentry = document.getElementById("myinput");
+ textentry.addEventListener("change", onChange);
+
+ function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
+ }
+
+
 
